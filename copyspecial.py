8fa32d9471d6117687c8d12548ba999be3154e63
Patrick Hanson <patrickhanson08@gmail.com>
2018-12-12 14:42:54 -0500

    Finished functionality

--- copyspecial.py
+++ copyspecial.py
@@ -6,36 +6,65 @@
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
-import sys
 import re
 import os
 import shutil
-import commands
+import subprocess
 import argparse
 
-"""Copy Special exercise
-"""
 
-# +++your code here+++
-# Write functions and modify main() to call them
+def collect_paths(directory):
+    file_directory = os.listdir(directory)
+    match_file = [os.path.abspath(file)
+                  for file in file_directory
+                  if re.search(r'__\w+__', file)]
+    return match_file
+
+
+def copy_files(list_of_paths, todir):
+    if os.path.exists(todir):
+        for file in list_of_paths:
+            shutil.copy(file, todir)
+            print """Added file: {file}
+            to new directory: {todir}""".format(file=file, todir=todir)
+    elif not os.path.exists(todir):
+        os.makedirs(todir)
+        for file in list_of_paths:
+            shutil.copy(file, todir)
+            print """Added file: {file}
+            to new directory: {todir}""".format(file=file, todir=todir)
+
+
+def zip_files(zipfile, list_of_paths):
+    zip_list = ['zip', '-j', zipfile]
+    zip_list.extend(list_of_paths)
+    print 'Command I am going to do:'
+    print ' '.join(zip_list)
+    """With Piero's assistance"""
+    try:
+        subprocess.check_output(zip_list)
+    except subprocess.CalledProcessError as e:
+        print e.output
+        exit(e.returncode)
+
 
 def main():
-    # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
+    parser.add_argument('from_dir', help='dir files are currently in')
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    # TODO need an argument to pick up 'from_dir'
     args = parser.parse_args()
 
-    # TODO you must write your own code to get the cmdline args.
-    # Read the docs and examples for the argparse module about how to do this.
+    collected_paths = collect_paths(args.from_dir)
+    if not collected_paths:
+        print 'no files match'
+    elif collected_paths and args.todir:
+        copy_files(collected_paths, args.todir)
+    elif args.tozip:
+        zip_files(args.tozip, collected_paths)
+    else:
+        print 'i am bamboozled'
 
-    # Parsing command line arguments is a must-have skill.
-    # This is input data validation.  If something is wrong (or missing) with any
-    # required args, the general rule is to print a usage message and exit(1).
 
-    # +++your code here+++
-    # Call your functions
-  
 if __name__ == "__main__":
     main()
